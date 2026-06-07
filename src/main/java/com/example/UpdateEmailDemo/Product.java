c73cfa018c38720eb51fb5a2d3843a943775dfa3
Hajar <hajar2000alfarsi@gmail.com>
2026-06-07 11:22:49 +0400

    Add lombok annotation

--- src/main/java/com/example/UpdateEmailDemo/Product.java
+++ src/main/java/com/example/UpdateEmailDemo/Product.java
@@ -1,8 +1,13 @@
 package com.example.UpdateEmailDemo;
 
+import lombok.Data;
+import lombok.Getter;
+
+@Data
 public class Product {
     private int id;
     private String name;
     private int stockQuantity;
+
 
 }
