0b90c3d80aaea919270a72589d29c2116d6a3b2a
Hajar <hajar2000alfarsi@gmail.com>
2026-06-07 10:56:46 +0400

    Create constructor, getters, setters, method to display userProfile

--- src/main/java/com/example/UpdateEmailDemo/UserProfile.java
+++ src/main/java/com/example/UpdateEmailDemo/UserProfile.java
@@ -5,4 +5,36 @@ public class UserProfile {
         private String phoneNumber;
         private String address;
 
+    public UserProfile(String phoneNumber, int userId, String address) {
+        this.phoneNumber = phoneNumber;
+        this.userId = userId;
+        this.address = address;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void displayProfile() {
+        System.out.println("User ID: " + userId);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Address: " + address);
+        System.out.println("------------------------");
+    }
 }
