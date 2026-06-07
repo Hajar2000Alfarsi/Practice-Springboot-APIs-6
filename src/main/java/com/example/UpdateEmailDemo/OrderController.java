9f565c6557630805c5e0d9be57ef076642b30a1f
Hajar <hajar2000alfarsi@gmail.com>
2026-06-07 11:58:51 +0400

    Create Order map

--- src/main/java/com/example/UpdateEmailDemo/OrderController.java
+++ src/main/java/com/example/UpdateEmailDemo/OrderController.java
@@ -2,6 +2,11 @@ package com.example.UpdateEmailDemo;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class OrderController {
+    private Map<Integer, Order> orderMap =  new HashMap<>();
+
 }
