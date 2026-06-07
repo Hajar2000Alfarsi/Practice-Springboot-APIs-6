f6613a4ce16cfc3da666ae5921fe57214223be63
Hajar <hajar2000alfarsi@gmail.com>
2026-06-07 11:53:42 +0400

    Add all needed fields for Order class with Constructor, Getter, Setter Anootation

--- src/main/java/com/example/UpdateEmailDemo/Order.java
+++ src/main/java/com/example/UpdateEmailDemo/Order.java
@@ -1,4 +1,15 @@
 package com.example.UpdateEmailDemo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Order {
+    private int orderId;
+    private String shippingAddress;
+    private String orderStatus;
+
 }
